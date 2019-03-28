6a82238778d4737735856d8cb76bb24b19afb606
Murilo Henrique <oofleaoo@gmail.com>
2019-03-27 21:35:51 -0300

    initial config and hello world

---
+++ desafio01/src/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+export default class App extends Component {
+  render() {
+    return <View style={styles.container}>Hello World</View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
