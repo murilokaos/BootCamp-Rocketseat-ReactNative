d98c6cd58023058a896389d9d03a5b97403f8f57
Murilo Henrique <oofleaoo@gmail.com>
2019-03-31 16:01:11 -0300

    the project never is finished

--- desafio01/src/index.js
+++ desafio01/src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 
 import '~/config/ReactotronConfig';
 import '~/config/ReactDevToolsConfig';
