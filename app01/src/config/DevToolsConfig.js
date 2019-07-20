451f5758782cb3ff67ddfc967a9586ec0c5a7193
Murilo Henrique <oofleaoo@gmail.com>
2019-07-20 11:28:39 -0300

    arrumando estrutura do git

--- app01/src/config/DevToolsConfig.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// eslint-disable-next-line no-undef
-if (__DEV__) {
-  // eslint-disable-next-line global-require
-  require('react-devtools');
-}
